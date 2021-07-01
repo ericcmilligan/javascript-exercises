0616e0661b39fc1714490b3a397d88f7974dff13
ericcmilligan <59842926+ericcmilligan@users.noreply.github.com>
2021-07-01 13:59:32 +0100

    Update getTheTitles.js

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,22 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
+
+const getTheTitles = function(object) {
+   for(array in object){
+     title = object[array]["title"] 
+     console.log(title)
+   }
 };
+
+getTheTitles(books)
 
 module.exports = getTheTitles;
